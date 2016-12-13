4a5d3c3eb7bacb36c925b7275a8b7902160a6348
mlotz <maciej.lotz@gmail.com>
2016-12-13 13:27:13 +0100

    m

--- msc_mlotz_pkg/src/plotterMatlab/plotter.m
+++ msc_mlotz_pkg/src/plotterMatlab/plotter.m
@@ -6,25 +6,55 @@ delimiterIn = ' ';
 headerlinesIn = 1;
 A = importdata(filename,delimiterIn,headerlinesIn);
 
-t = A.data(:,1);
-X = A.data(:,2);
-Y = A.data(:,3);
+xFN = A.data(:,1);
+yFN= A.data(:,2);
+zFN = A.data(:,3);
+t = str2double(A.textdata(2:end,1));
+X = str2double(A.textdata(2:end,2));
+Y = str2double(A.textdata(2:end,3));
+xF = str2double(A.textdata(2:end,17));
+yF = str2double(A.textdata(2:end,18));
+zF = str2double(A.textdata(2:end,19));
 
-f1 = figure(1);
+
+% f1 = figure(1);
+% hold on;
+% title('Trajektoria XY');
+% xlabel('X');
+% ylabel('Y');
+% plot(X,Y);
+% f2 = figure(2);
+% hold on;
+% title('X w czasie');
+% xlabel('t');
+% ylabel('X');
+% plot(t,X);
+% f3 = figure(3);
+% hold on;
+% title('Y w czasie');
+% xlabel('t');
+% ylabel('Y');
+% plot(t,Y);
+
+f4 = figure(4);
 hold on;
-title('Trajektoria XY');
-xlabel('X');
-ylabel('Y');
-plot(X,Y);
-f2 = figure(2);
-hold on;
-title('X w czasie');
+title('optForce2 - xFN');
 xlabel('t');
-ylabel('X');
-plot(t,X);
-f3 = figure(3);
+ylabel('xFN');
+plot(t,xFN);
+
+f5 = figure(5);
 hold on;
-title('Y w czasie');
+title('optForce2 - yFN');
 xlabel('t');
-ylabel('Y');
-plot(t,Y);
+ylabel('yFN');
+plot(t,yFN);
+
+f6 = figure(6);
+hold on;
+title('optForce2 - zFN');
+xlabel('t');
+ylabel('zFN');
+plot(t,zFN);
+
+
